9c000dd7b3d95e6518008e5addb66b67a36f9501
sungmin yang <33611799+sugminyang@users.noreply.github.com>
2022-03-15 16:40:30 +0900

    submit an assignement1
     4 / 5 solved..

---
+++ lecture01/mini_project.java
@@ -0,0 +1,10 @@
+package lecture01;
+
+public class mini_project {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요? \n 저는 프로그래밍이 처음이예요:)\nBye!!");
+
+	}
+
+}
---
+++ lecture01/problem1.java
@@ -0,0 +1,5 @@
+package lecture01;
+
+public class problem1 {
+
+}
